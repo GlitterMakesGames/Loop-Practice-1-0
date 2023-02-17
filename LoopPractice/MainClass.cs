5ac9cc6162ea51e2250da66311084ce7b32da346
GlitterMakesGames <123193351+GlitterMakesGames@users.noreply.github.com>
2023-02-17 12:23:28 -0600

    First draft for week 5 assignment
    My code is not finished just yet! Stay tuned for the updated version!

---
+++ LoopPractice/MainClass.cs
@@ -0,0 +1,77 @@
+﻿using static MathToolkit;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string input = "";
+        string output = "";
+        int temp;
+        bool win = false;
+
+        //Problem #1
+        Console.Write("Please enter a positive integer: ");
+        int dividend = Int32.Parse(Console.ReadLine());
+        output = divisibleByThree(dividend) + "";
+        Console.WriteLine("There are " + output + " numbers divisible by three between 1 and " + dividend);
+        Console.WriteLine();
+
+        //Problem #2
+        Console.WriteLine("Please enter all the integers you would like to add (maximum of 10).");
+        Console.WriteLine("One at a time, please. Otherwise, enter \"ok\" to move on.\n");
+        for(int i = 0; i < 10; i++)
+        {
+            Console.Write("Enter the Next Number: ");
+            input = Console.ReadLine();
+
+            if (input.Equals("ok", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+            temp = Int32.Parse(input);
+            output = sumAllInputs(temp) + "";
+            Console.WriteLine("The total sum is now " + output);
+
+            if (i == 9)
+            {
+                Console.WriteLine("\nYou have used up all your numbers.\n");
+            }
+        }
+
+        //Problem #3
+        Console.Write("Please enter an integer for the factorial calculation: ");
+        input = Console.ReadLine();
+        output = factorial(Int32.Parse(input)) + "";
+        Console.WriteLine(input + "! is equal to " + output);
+
+        //Problem #4
+        getRandNum();
+        for (int attempts = 0; attempts < 4; attempts++)
+        {
+            Console.Write("Enter your guess (1-10): ");
+            input = Console.ReadLine();
+            if (guessingGame(Int32.Parse(input)))
+            {
+                win = true;
+                Console.WriteLine("You Win!");
+            }
+            else
+            {
+                win = false;
+                Console.WriteLine("Incorrect!");
+            }
+        }
+        if (!win)
+        {
+            Console.WriteLine("You Lose!");
+        }
+
+        //Problem #5
+        Console.WriteLine("Please enter a list of integers separated by commas.");
+        Console.WriteLine("Press \"Enter\" when done.");
+        Console.Write("Type Here: ");
+        input = Console.ReadLine();
+        output = getBiggestNum(input) + "";
+        Console.WriteLine("The biggest number in the list provided is " + output);
+    }
+}
---
+++ LoopPractice/MathToolkit.cs
@@ -0,0 +1,113 @@
+﻿public class MathToolkit
+{
+    //For Problem #2
+    private static int[] sumArray = new int[10];
+    private static int sumArray_Index = 0;
+    private static bool firstIteration = true;
+    private static int tempIndex = 0;
+    //For Problem #4
+    private static Random rng = new Random();
+    private static int chosenNum = 0;
+    //For Problem #5
+    private static string[] numList_String;
+    private static int[] numList_Int;
+    private static int num;
+    private static int counter = 0;
+    private static int biggest;
+    public static int divisibleByThree(int dividend)
+    {
+        int count = 0;
+        for(int i = 1; i <= dividend; i++)
+        {
+            if ((i%3) == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int sumAllInputs(int addend)
+    {
+        if(firstIteration)
+        {
+            if(sumArray_Index <= 9)
+            {
+                sumArray[sumArray_Index] = addend;
+            }
+            tempIndex = sumArray_Index;
+            sumArray_Index++;
+            firstIteration = false;
+        }
+
+        if (tempIndex > 0)
+        {
+            return addend + sumAllInputs(sumArray[--tempIndex]);
+        }
+        else
+        {
+            firstIteration = true;
+            return addend;
+        }
+    }
+
+    public static int factorial(int facBase)
+    {
+        if (facBase == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return facBase * factorial(--facBase);
+        }
+    }
+
+    public static void getRandNum()
+    {
+        chosenNum = rng.Next(1, 11);
+    }
+
+    public static bool guessingGame(int guess)
+    {
+        if (guess == chosenNum)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //WARNING! DOES NOT ACCOUNT FOR NUMBERS WITH MORE THAN ONE DIGIT!
+    //Replace foreach loop with for loop
+    //Iterate through a nested loop (but keep incrementing the outer loop variable)
+    //until a comma is found (use ASCII)
+    public static int getBiggestNum(string input)
+    {
+        numList_String = new string[input.Length];
+        foreach (char c in input)
+        {
+            if (Int32.TryParse(c + "", out num))
+            {
+                numList_String[counter] = c + "";
+                counter++;
+            }
+        }
+        numList_Int = new int[counter];
+        for (int i = 0; i < counter; i++)
+        {
+            numList_Int[i] = Int32.Parse(numList_String[i]);
+        }
+        biggest = numList_Int[0];
+        for(int i = 1; i < counter; i++)
+        {
+            if (numList_Int[i] > biggest)
+            {
+                biggest = numList_Int[i];
+            }
+        }
+        return biggest;
+    }
+}
